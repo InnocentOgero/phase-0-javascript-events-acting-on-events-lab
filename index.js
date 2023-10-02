c64815162dfed6dee5d47670e2e4a47e693996b3
InnocentOgero <innocent.ogero@student.moringaschool.com>
2023-10-02 16:59:48 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Your code here
+// Your code herefunction moveDodgerLeft() {
+    function moveDodgerLeft() {
+      const leftnumbers=dodger.style.left.replace('px','')
+      const leftPosition = parseInt(leftnumbers,10)
+      if (leftPosition > 0) {
+          dodger.style.left = `${leftPosition - 1}px`;
+        }
+      }
+
+function moveDodgerRight() {
+  const leftnumbers= dodger.style.left.replace('px','')
+  const leftPosition=parseInt(leftnumbers,10)
+  const gameFieldWidth=document.documentElement.clientWidth;
+  let dodgerWidth=40;
+  const maxLeftPosition = gameFieldWidth - dodgerWidth;
+  dodger.style.left = `${leftPosition + 1}px`;
+  
+}
+
